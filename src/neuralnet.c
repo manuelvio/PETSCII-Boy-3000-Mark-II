ad7fa4877ce9193f968d59099c42857a77635d6b
Manuel Vio <manuelvio74@gmail.com>
2025-06-09 19:03:00 +0200

    Added LICENSE

--- src/neuralnet.c
+++ src/neuralnet.c
@@ -2,6 +2,28 @@
 #include <limits.h>
 #include "neuralnet.h"
 
+/*
+MIT License
+
+Copyright (c) 2025-Present Manuel Vio
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 // Extract specific bit value from an array of 32 bytes, index can range from 0 to 255
 // Here we treat the byte array as a bit stream, or a linear representation of an input matrix
 // where dimensions are 16x16 boolean values
